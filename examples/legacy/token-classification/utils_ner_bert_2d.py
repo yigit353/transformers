0a10f477151ba626f91f24ae92e6add18d7e43aa
Yiğit Bekir Kaya <yigit353@gmail.com>
2023-01-25 17:18:19 +0300

    Update utils_ner_bert_2d.py
    Changed it to process single example

--- examples/legacy/token-classification/utils_ner_bert_2d.py
+++ examples/legacy/token-classification/utils_ner_bert_2d.py
@@ -84,8 +84,8 @@ class TokenClassificationTask:
         raise NotImplementedError
 
     @staticmethod
-    def convert_examples_to_features(
-            examples: List[InputExample],
+    def convert_example_to_feature(
+            example: InputExample,
             label_list: List[str],
             max_seq_length: int,
             tokenizer: PreTrainedTokenizer,
@@ -107,116 +107,100 @@ class TokenClassificationTask:
             - True (XLNet/GPT pattern): A + [SEP] + B + [SEP] + [CLS]
         `cls_token_segment_id` define the segment id associated to the CLS token (0 for BERT, 2 for XLNet)
         """
-        # TODO clean up all this to leverage built-in features of tokenizers
-
         label_map = {label: i for i, label in enumerate(label_list)}
 
         features = []
-        for ex_index, example in enumerate(examples):
-            if ex_index % 10_000 == 0:
-                logger.info("Writing example %d of %d", ex_index, len(examples))
 
-            tokens = []
-            label_ids = []
-            for word, label in zip(example.words, example.labels):
-                word_tokens = tokenizer.tokenize(word)
+        tokens = []
+        label_ids = []
+        for word, label in zip(example.words, example.labels):
+            word_tokens = tokenizer.tokenize(word)
 
-                # bert-base-multilingual-cased sometimes output "nothing ([]) when calling tokenize with just a space.
-                if len(word_tokens) > 0:
-                    tokens.extend(word_tokens)
-                    # Use the real label id for the first token of the word, and padding ids for the remaining tokens
-                    label_ids.extend([label_map[label]] + [pad_token_label_id] * (len(word_tokens) - 1))
+            # bert-base-multilingual-cased sometimes output "nothing ([]) when calling tokenize with just a space.
+            if len(word_tokens) > 0:
+                tokens.extend(word_tokens)
+                # Use the real label id for the first token of the word, and padding ids for the remaining tokens
+                label_ids.extend([label_map[label]] + [pad_token_label_id] * (len(word_tokens) - 1))
 
-            # Account for [CLS] and [SEP] with "- 2" and with "- 3" for RoBERTa.
-            special_tokens_count = tokenizer.num_special_tokens_to_add()
-            if len(tokens) > max_seq_length - special_tokens_count:
-                tokens = tokens[: (max_seq_length - special_tokens_count)]
-                label_ids = label_ids[: (max_seq_length - special_tokens_count)]
+        # Account for [CLS] and [SEP] with "- 2" and with "- 3" for RoBERTa.
+        special_tokens_count = tokenizer.num_special_tokens_to_add()
+        if len(tokens) > max_seq_length - special_tokens_count:
+            tokens = tokens[: (max_seq_length - special_tokens_count)]
+            label_ids = label_ids[: (max_seq_length - special_tokens_count)]
 
-            # The convention in BERT is:
-            # (a) For sequence pairs:
-            #  tokens:   [CLS] is this jack ##son ##ville ? [SEP] no it is not . [SEP]
-            #  type_ids:   0   0  0    0    0     0       0   0   1  1  1  1   1   1
-            # (b) For single sequences:
-            #  tokens:   [CLS] the dog is hairy . [SEP]
-            #  type_ids:   0   0   0   0  0     0   0
-            #
-            # Where "type_ids" are used to indicate whether this is the first
-            # sequence or the second sequence. The embedding vectors for `type=0` and
-            # `type=1` were learned during pre-training and are added to the wordpiece
-            # embedding vector (and position vector). This is not *strictly* necessary
-            # since the [SEP] token unambiguously separates the sequences, but it makes
-            # it easier for the model to learn the concept of sequences.
-            #
-            # For classification tasks, the first vector (corresponding to [CLS]) is
-            # used as the "sentence vector". Note that this only makes sense because
-            # the entire model is fine-tuned.
+        # The convention in BERT is:
+        # (a) For sequence pairs:
+        #  tokens:   [CLS] is this jack ##son ##ville ? [SEP] no it is not . [SEP]
+        #  type_ids:   0   0  0    0    0     0       0   0   1  1  1  1   1   1
+        # (b) For single sequences:
+        #  tokens:   [CLS] the dog is hairy . [SEP]
+        #  type_ids:   0   0   0   0  0     0   0
+        #
+        # Where "type_ids" are used to indicate whether this is the first
+        # sequence or the second sequence. The embedding vectors for `type=0` and
+        # `type=1` were learned during pre-training and are added to the wordpiece
+        # embedding vector (and position vector). This is not *strictly* necessary
+        # since the [SEP] token unambiguously separates the sequences, but it makes
+        # it easier for the model to learn the concept of sequences.
+        #
+        # For classification tasks, the first vector (corresponding to [CLS]) is
+        # used as the "sentence vector". Note that this only makes sense because
+        # the entire model is fine-tuned.
+        tokens += [sep_token]
+        label_ids += [pad_token_label_id]
+        if sep_token_extra:
+            # roberta uses an extra separator b/w pairs of sentences
             tokens += [sep_token]
             label_ids += [pad_token_label_id]
-            if sep_token_extra:
-                # roberta uses an extra separator b/w pairs of sentences
-                tokens += [sep_token]
-                label_ids += [pad_token_label_id]
-            segment_ids = [sequence_a_segment_id] * len(tokens)
+        segment_ids = [sequence_a_segment_id] * len(tokens)
 
-            if cls_token_at_end:
-                tokens += [cls_token]
-                label_ids += [pad_token_label_id]
-                segment_ids += [cls_token_segment_id]
-            else:
-                tokens = [cls_token] + tokens
-                label_ids = [pad_token_label_id] + label_ids
-                segment_ids = [cls_token_segment_id] + segment_ids
+        if cls_token_at_end:
+            tokens += [cls_token]
+            label_ids += [pad_token_label_id]
+            segment_ids += [cls_token_segment_id]
+        else:
+            tokens = [cls_token] + tokens
+            label_ids = [pad_token_label_id] + label_ids
+            segment_ids = [cls_token_segment_id] + segment_ids
 
-            input_ids = tokenizer.convert_tokens_to_ids(tokens)
+        input_ids = tokenizer.convert_tokens_to_ids(tokens)
 
-            # The mask has 1 for real tokens and 0 for padding tokens. Only real
-            # tokens are attended to.
-            input_mask = [1 if mask_padding_with_zero else 0] * len(input_ids)
+        # The mask has 1 for real tokens and 0 for padding tokens. Only real
+        # tokens are attended to.
+        input_mask = [1 if mask_padding_with_zero else 0] * len(input_ids)
 
-            # Zero-pad up to the sequence length.
-            padding_length = max_seq_length - len(input_ids)
-            if pad_on_left:
-                input_ids = ([pad_token] * padding_length) + input_ids
-                input_mask = ([0 if mask_padding_with_zero else 1] * padding_length) + input_mask
-                segment_ids = ([pad_token_segment_id] * padding_length) + segment_ids
-                label_ids = ([pad_token_label_id] * padding_length) + label_ids
-            else:
-                input_ids += [pad_token] * padding_length
-                input_mask += [0 if mask_padding_with_zero else 1] * padding_length
-                segment_ids += [pad_token_segment_id] * padding_length
-                label_ids += [pad_token_label_id] * padding_length
+        # Zero-pad up to the sequence length.
+        padding_length = max_seq_length - len(input_ids)
+        if pad_on_left:
+            input_ids = ([pad_token] * padding_length) + input_ids
+            input_mask = ([0 if mask_padding_with_zero else 1] * padding_length) + input_mask
+            segment_ids = ([pad_token_segment_id] * padding_length) + segment_ids
+            label_ids = ([pad_token_label_id] * padding_length) + label_ids
+        else:
+            input_ids += [pad_token] * padding_length
+            input_mask += [0 if mask_padding_with_zero else 1] * padding_length
+            segment_ids += [pad_token_segment_id] * padding_length
+            label_ids += [pad_token_label_id] * padding_length
 
-            word_position_ids = tokenizer.create_word_ids_from_sequence(input_ids)
-            subword_ids = tokenizer.create_subword_ids_from_sequence(input_ids)
+        word_position_ids = tokenizer.create_word_ids_from_sequence(input_ids)
+        subword_ids = tokenizer.create_subword_ids_from_sequence(input_ids)
 
-            assert len(input_ids) == max_seq_length
-            assert len(input_mask) == max_seq_length
-            assert len(segment_ids) == max_seq_length
-            assert len(word_position_ids) == max_seq_length
-            assert len(subword_ids) == max_seq_length
-            assert len(label_ids) == max_seq_length
+        assert len(input_ids) == max_seq_length
+        assert len(input_mask) == max_seq_length
+        assert len(segment_ids) == max_seq_length
+        assert len(word_position_ids) == max_seq_length
+        assert len(subword_ids) == max_seq_length
+        assert len(label_ids) == max_seq_length
 
-            if ex_index < 5:
-                logger.info("*** Example ***")
-                logger.info("guid: %s", example.guid)
-                logger.info("tokens: %s", " ".join([str(x) for x in tokens]))
-                logger.info("input_ids: %s", " ".join([str(x) for x in input_ids]))
-                logger.info("input_mask: %s", " ".join([str(x) for x in input_mask]))
-                logger.info("segment_ids: %s", " ".join([str(x) for x in segment_ids]))
-                logger.info("word_position_ids: %s", " ".join([str(x) for x in word_position_ids]))
-                logger.info("subword_ids: %s", " ".join([str(x) for x in subword_ids]))
-                logger.info("label_ids: %s", " ".join([str(x) for x in label_ids]))
+        if "token_type_ids" not in tokenizer.model_input_names:
+            segment_ids = None
 
-            if "token_type_ids" not in tokenizer.model_input_names:
-                segment_ids = None
-
-            features.append(
-                InputFeatures(
-                    input_ids=input_ids, attention_mask=input_mask, token_type_ids=segment_ids,
-                    word_position_ids=word_position_ids, subword_ids=subword_ids, label_ids=label_ids,
-                )
+        features.append(
+            InputFeatures(
+                input_ids=input_ids, attention_mask=input_mask, token_type_ids=segment_ids,
+                word_position_ids=word_position_ids, subword_ids=subword_ids, label_ids=label_ids,
             )
+        )
         return features
 
 
@@ -271,7 +255,7 @@ if is_torch_available():
                     threads = min(threads, cpu_count())
                     with Pool(threads) as p:
                         annotate_ = partial(
-                            token_classification_task.convert_examples_to_features,
+                            token_classification_task.convert_example_to_feature,
                             label_list=labels,
                             max_seq_length=max_seq_length,
                             tokenizer=tokenizer,
